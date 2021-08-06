0f185cff2bb19dfa72f2150d665bba1659c00bd7
Mariano Cordoba <cordobam1987@gmail.com>
2021-08-06 20:40:22 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USERNAME= 'cordobam1987@gmail.com'
-    MAIL_PASSWORD = 'lingncabewtazqki'
+    MAIL_PASSWORD = '############'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
